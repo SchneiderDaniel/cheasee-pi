66b2bc807c2663f00610f50afc93e4857ca82a3d
SchneiderDaniel <daniel.schneider.privat@googlemail.com>
2026-06-23 21:04:28 +0200

    Update duplicate-code.ts

--- .pi/extensions/supervisor/checks/duplicate-code.ts
+++ .pi/extensions/supervisor/checks/duplicate-code.ts
@@ -115,13 +115,6 @@ function cloneFiles(clone: JscpdClone): string[] {
 }
 
 /**
- * Extract file paths from a jscpd clone's fragments.
- */
-function cloneFiles(clone: JscpdClone): string[] {
-	return clone.fragments.map((frag) => frag.file);
-}
-
-/**
  * Filter jscpd clones to only those where at least one location
  * is in the changed files list.
  */
