63a50267ca60b12ed26c65fb214243850e157852
SchneiderDaniel <11504368+SchneiderDaniel@users.noreply.github.com>
2026-07-22 22:09:16 +0200

    chore: bump version to v0.33

--- cmd/cheasee-pi/root_test.go
+++ cmd/cheasee-pi/root_test.go
@@ -154,7 +154,7 @@ func TestRootCmd_Version_IsValidSemver(t *testing.T) {
 }
 
 func TestRootCmd_Version_IsExpectedRelease(t *testing.T) {
-	expected := "0.32"
+	expected := "0.33"
 	if rootCmd.Version != expected {
 		t.Errorf("rootCmd.Version = %q, want %q", rootCmd.Version, expected)
 	}
