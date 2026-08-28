0fac4bf06da4a8329cddc69e5ec945e5bf16876f
DanielSchneider <daniel.schneider.privat@googlemail.com>
2026-08-28 21:10:18 +0000

    chore: bump version to v0.55.2

--- cmd/cheasee-pi/root_test.go
+++ cmd/cheasee-pi/root_test.go
@@ -144,7 +144,7 @@ func TestRootCmd_Version_IsValidSemver(t *testing.T) {
 }
 
 func TestRootCmd_Version_IsExpectedRelease(t *testing.T) {
-	expected := "0.55.1"
+	expected := "0.55.2"
 	if rootCmd.Version != expected {
 		t.Errorf("rootCmd.Version = %q, want %q", rootCmd.Version, expected)
 	}
